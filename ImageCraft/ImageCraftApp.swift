c56baad3ab3b871d365d61c6f83c97d6263759a8
Himanshu Goyal <goyahim69@gmail.com>
2024-10-02 20:26:11 +0530

    Initial Commit

---
+++ ImageCraft/ImageCraftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageCraftApp.swift
+//  ImageCraft
+//
+//  Created by Himanshu's MacBook on 02/10/24.
+//
+
+import SwiftUI
+
+@main
+struct ImageCraftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
